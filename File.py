cbadb09e804c5033d55ad93760643a7bb564ad8a
manaschawla <manaschawla324@gmail.com>
2024-08-08 12:38:22 +0530

    Delete File.py

--- File.py
+++
@@ -1,9 +0,0 @@
-filename = 'manas.txt'
-with open(filename, 'w') as file:
-    file.write("i am manas. \n i am 18 years old")
-with open(filename, 'a') as file:
-    file.write("\ni am currently pursuing b.tech")
-    
-with open(filename, 'r') as file:
-   content =  file.read()
-print(content)
